3f0c44206591c8427ad0fb5699f403d6a1b6b6c4
JPCabral04 <jpqcabral04@gmail.com>
2025-01-27 16:25:39 -0300

    add sweetalert2 for confirmation dialogs; implement input-list-item component for task management

--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,36 +1,129 @@
 import { Component, signal } from '@angular/core';
+import Swal from 'sweetalert2';
 
-//Components
+// Components
 import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
+import { InputListItemComponent } from '../../components/input-list-item/input-list-item.component';
 
-//Interface
+//Enun
+import { ELocalStorage } from '../../enum/ELocalStroragem.enum';
+
+//interface
 import { IListItems } from '../../interface/IListItems.interface';
+
+
 
 @Component({
   selector: 'app-list',
-  imports: [InputAddItemComponent],
+  standalone: true,
+  imports: [InputAddItemComponent, InputListItemComponent],
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
 export class ListComponent {
   public addItem = signal(true);
 
-  private listItems = signal<IListItems[]>(this.loadItems());
-  public items = this.listItems.asReadonly();
+  #setListItems = signal<IListItems[]>(this.#parseItems());
+  public getListItems = this.#setListItems.asReadonly();
 
-  private loadItems(): IListItems[] {
-    return JSON.parse(localStorage.getItem('@my-list') || '[]');
+  #parseItems() {
+    return JSON.parse(localStorage.getItem(ELocalStorage.MY_LIST) || '[]');
   }
 
-  public getInputAndAddItem(value: IListItems): void {
-    const updatedItems = [...this.listItems(), value];
-    localStorage.setItem('@my-list', JSON.stringify(updatedItems));
-    this.listItems.set(updatedItems);
+  #updateLocalStorage() {
+    return localStorage.setItem(
+      ELocalStorage.MY_LIST,
+      JSON.stringify(this.#setListItems())
+    );
   }
 
-  public deleteAllItems(){
-    localStorage.removeItem('@my-list');
-    this.listItems.set(this.loadItems());
-  }
-};
+  public getInputAndAddItem(value: IListItems) {
+    localStorage.setItem(
+      ELocalStorage.MY_LIST,
+      JSON.stringify([...this.#setListItems(), value])
+    );
 
+    return this.#setListItems.set(this.#parseItems());
+  }
+
+  public listItemsStage(value: 'pending' | 'completed') {
+    return this.getListItems().filter((res: IListItems) => {
+      if (value === 'pending') {
+        return !res.checked;
+      }
+
+      if (value === 'completed') {
+        return res.checked;
+      }
+
+      return res;
+    });
+  }
+
+  public updateItemCheckbox(newItem: { id: string; checked: boolean }) {
+    this.#setListItems.update((oldValue: IListItems[]) => {
+      oldValue.filter((res) => {
+        if (res.id === newItem.id) {
+          res.checked = newItem.checked;
+          return res;
+        }
+
+        return res;
+      });
+
+      return oldValue;
+    });
+
+    return this.#updateLocalStorage();
+  }
+
+  public updateItemText(newItem: { id: string; value: string }) {
+    this.#setListItems.update((oldValue: IListItems[]) => {
+      oldValue.filter((res) => {
+        if (res.id === newItem.id) {
+          res.value = newItem.value;
+          return res;
+        }
+
+        return res;
+      });
+
+      return oldValue;
+    });
+
+    return this.#updateLocalStorage();
+  }
+
+  public deleteItem(id: string) {
+    Swal.fire({
+      title: 'Tem certeza?',
+      text: 'Você não poderá reverter isso!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sim, delete o item',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.#setListItems.update((oldValue: IListItems[]) => {
+          return oldValue.filter((res) => res.id !== id);
+        });
+
+        return this.#updateLocalStorage();
+      }
+    });
+  }
+
+  public deleteAllItems() {
+    Swal.fire({
+      title: 'Tem certeza?',
+      text: 'Você não poderá reverter isso!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Sim, delete tudo',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem(ELocalStorage.MY_LIST);
+        return this.#setListItems.set(this.#parseItems());
+      }
+    });
+  }
+}
